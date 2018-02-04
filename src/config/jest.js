9b96f8a1657ca12315151f1200121c8f85ea7220
Sid Doshi <sidoshi510@gmail.com>
2018-02-04 15:20:47 +0530

    Add babel transforms to jest

--- src/config/jest.js
+++ src/config/jest.js
@@ -1,4 +1,7 @@
+const fs = require('fs')
 const paths = require('../utils/paths')
+
+const useDefaultConfig = !fs.existsSync(paths.babelrc)
 
 module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
@@ -8,6 +11,12 @@ module.exports = {
     '<rootDir>/src/**/?(*.)(spec|test).js',
   ],
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': useDefaultConfig
+      ? require.resolve('./babelTransform.js')
+      : require.resolve('babel-jest'),
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   coveragePathIgnorePatterns: ['/__fixtures__/'],
   rootDir: paths.appDirectory,
 }
